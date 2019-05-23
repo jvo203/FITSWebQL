73b913ad89be45f24336c3de5cd44a0bd604dfbd
jvo203 <chris.zapart@nao.ac.jp>
2019-05-23 17:49:09 +0900

    testing RBF compression with OpenCL

--- Julia/fitswebql.jl
+++ Julia/fitswebql.jl
@@ -2,16 +2,25 @@ using AstroImages
 #] pkg> add https://github.com/JuliaAstro/AstroImages.jl
 
 using FITSIO
+using NaNMath; nm = NaNMath
+using OpenCL
 using Plots
+using Random
 using Statistics
 using Wavelets
 
 dir = "/home/chris/ダウンロード"
-file = "e20121211_0010500001_dp_sf_st_mos.fits"
+#file = "e20121211_0010500001_dp_sf_st_mos.fits"
+#file = "ALMA01030862.fits"
+file = "ALMA01157085.fits"
+#file = "Cygnus_sp46_vs-150_ve100_dv0.50_CN_Tmb.fits.gz" #good for testing noisy datasets
+
+#dir = "/home/chris/NAO/NRO/SF/"
 
 fitspath = string(dir, "/", file)
-fitspath = "/home/chris/NAO/SubaruWebQL/FITSCACHE/SUPM139C52520E922500.fits"
-fitspath = "/home/chris/projects/fits_web_ql/FITSCACHE/d61977e0-fada-3cee-8198-31c00ea64ab3.fits"
+
+#fitspath = "/home/chris/NAO/SubaruWebQL/FITSCACHE/SUPM139C52520E922500.fits"
+#fitspath = "/home/chris/projects/fits_web_ql/FITSCACHE/d61977e0-fada-3cee-8198-31c00ea64ab3.fits"
 
 println(ARGS, "\targc:", size(ARGS)[1])
 
@@ -32,7 +41,7 @@ println(fits[1])
 data = @time read(fits[1])
 dim = minimum(size(data))
 println(size(data), " the smaller dim:", dim)
-range = @time extrema(data)
+range = @time nm.extrema(data)
 println("min,max:", range)
 med = @time median(data)
 println("median:", med)
@@ -52,3 +61,62 @@ wt = wavelet(WT.db2, WT.Lifting)
 L = 4
 @time wpt(sqdata, wt, L)
 =#
+
+dims = size(data)
+width = dims[1]
+height = dims[2]
+depth = dims[3]
+capacity = width * height
+
+XCLUST = Int(round(width / 16))
+YCLUST = Int(round(height / 16))
+NCLUST = XCLUST * YCLUST
+
+println("width : ", width, "\theight : ", height, "\tdepth : ", depth, "\tcapacity : ", capacity)
+println("XCLUST : ", XCLUST, "\tYCLUST : ", YCLUST, "\tNCLUST : ", NCLUST)
+
+device, ctx, queue = cl.create_compute_context()
+println(device)
+
+compression_code = open("rbf.cl") do file
+    #define NCLUST dynamically
+    "#define NCLUST $(NCLUST)\n" * read(file, String)
+end
+
+program = cl.Program(ctx, source = compression_code) |> cl.build!
+
+for frame = 1:1#1:depth
+    sub = view(data, :, :, frame, 1)
+    println("frame : ", frame, "\tdims: ", size(sub))
+    (frame_min, frame_max) = @time nm.extrema(sub)
+
+    x1 = Float32[]
+    x2 = Float32[]
+    d = Float32[]
+    y = Float32[]
+    e = Float32[]
+    count = 0
+
+    for iy = 1:height
+        for ix = 1:width
+            tmp = sub[ix,iy]
+            if !isnan(tmp)
+                count = count + 1
+                pixel = log(0.5 + (tmp - frame_min) / (frame_max - frame_min))
+                push!(d, pixel)
+                push!(x1, (ix - 1) / (width - 1))   #[0,1]
+                push!(x2, (iy - 1) / (height - 1))  #[0,1]
+                push!(y, 0)
+                push!(e, 0)
+            end
+        end
+    end
+
+    println("count : ", count)
+    
+    #training data OK
+
+    #parameter initialisation
+    c1 = shuffle(x1)[1:NCLUST]
+    c2 = shuffle(x2)[1:NCLUST]    
+end
---
+++ Julia/rbf.cl
@@ -0,0 +1,85 @@
+inline void atomicAdd_l_f(volatile __local float *addr, float val)
+{
+    union {
+        unsigned int u32;
+        float f32;
+    } next, expected, current;
+    current.f32 = *addr;
+    do
+    {
+        expected.f32 = current.f32;
+        next.f32 = expected.f32 + val;
+        current.u32 = atomic_cmpxchg((volatile __local unsigned int *)addr,
+                                     expected.u32, next.u32);
+    } while (current.u32 != expected.u32);
+}
+
+inline void atomicAdd_g_f(volatile __global float *addr, float val)
+{
+    union {
+        unsigned int u32;
+        float f32;
+    } next, expected, current;
+    current.f32 = *addr;
+    do
+    {
+        expected.f32 = current.f32;
+        next.f32 = expected.f32 + val;
+        current.u32 = atomic_cmpxchg((volatile __global unsigned int *)addr,
+                                     expected.u32, next.u32);
+    } while (current.u32 != expected.u32);
+}
+
+__kernel void rbf_forward_pass(__global float *_x1, __global float *_x2, __global float *_y, __global float *_data, __global float *_e, __constant float *c1, __constant float *c2, __constant float *p0, __constant float *p1, __constant float *p2, __constant float *w, __global float *_grad_w)
+{
+
+    __local float tid_grad_w[NCLUST + 1];
+    float grad_w[NCLUST + 1];
+
+    size_t local_index = get_local_id(0);
+
+    if (local_index == 0)
+    {
+        for (int i = 0; i < NCLUST + 1; i++)
+            tid_grad_w[i] = 0.0;
+    };
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    size_t index = get_global_id(0);
+
+    float x1 = _x1[index];
+    float x2 = _x2[index];
+
+    float tmp = w[NCLUST];
+    grad_w[NCLUST] = 1.0; //bias
+
+    for (int i = 0; i < NCLUST; i++)
+    {
+        float a = native_exp(p0[i]);
+        float b = p1[i];
+        float c = native_exp(p2[i]);
+
+        float tmp1 = (x1 - c1[i]);
+        float tmp2 = (x2 - c2[i]);
+        float dist = a * tmp1 * tmp1 - 2.0f * b * tmp1 * tmp2 + c * tmp2 * tmp2;
+        float act = native_exp(-dist);
+
+        tmp += w[i] * act;
+        grad_w[i] = act;
+    }
+
+    float e = tmp - _data[index];
+    _y[index] = tmp;
+    _e[index] = e;
+
+    //gradients
+    for (int i = 0; i < NCLUST + 1; i++)
+        atomicAdd_l_f(&(tid_grad_w[i]), e * grad_w[i]);
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_index == (get_local_size(0) - 1))
+    {
+        for (int i = 0; i < NCLUST + 1; i++)
+            atomicAdd_g_f(&(_grad_w[i]), tid_grad_w[i]);
+    }
+}
