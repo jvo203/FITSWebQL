7e6e69f34ff3ed0acc9ffd24fb136ab2c7089f3a
jvo203 <chris.zapart@nao.ac.jp>
2019-11-08 16:23:44 +0900

    modified:   src/fits.hpp

--- src/fits.hpp
+++ src/fits.hpp
@@ -57,9 +57,9 @@ public:
 
 public:
   void update_timestamp();
-  void from_url(std::string url, std::string flux, int va_count, boost::shared_ptr<shared_state> const& state);
+  void from_url(std::string url, std::string flux, int va_count/*, boost::shared_ptr<shared_state> const& state*/);
   void from_path_zfp(std::string path, bool is_compressed, std::string flux,
-                     int va_count, boost::shared_ptr<shared_state> const& state);
+                     int va_count/*, boost::shared_ptr<shared_state> const& state*/);
   void get_frequency_range(double &freq_start, double &freq_end);
   void to_json(std::ostringstream &json);
 
