373b7681489f67a080d739afc142a207f75e4e1c
jvo203 <chris.zapart@nao.ac.jp>
2019-11-12 17:25:35 +0900

    modified:   src/main_mg.cpp

--- src/main_mg.cpp
+++ src/main_mg.cpp
@@ -770,7 +770,7 @@ static void ev_handler(struct mg_connection *nc, int ev, void *ev_data)
 					  return execute_fits(nc, dir, ext, db, table, datasets, composite, flux);
         }  
 
-        mg_http_send_error(nc, 404, NULL);        
+        mg_http_send_error(nc, 404, NULL);     
         break;
       }
 
