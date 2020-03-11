e8f675c4b4a244528c5afa6a4dd66a5b3d69cd00
jvo203 <chris.zapart@nao.ac.jp>
2020-03-11 18:01:54 +0900

    modified:   src/fits.cpp

--- src/fits.cpp
+++ src/fits.cpp
@@ -2982,8 +2982,8 @@ IppStatus tileResize32f_C1R(Ipp32f *pSrc, IppiSize srcSize, Ipp32s srcStep,
                                              &srcOffset, &srcSizeT);
 
         if (pStatus[i] == ippStsNoErr) {
-          pSrcT = (Ipp32f *)((char *)pSrc + srcOffset.y * srcStep);
-          pDstT = (Ipp32f *)((char *)pDst + dstOffset.y * dstStep);
+          pSrcT = (Ipp32f *)(pSrc + srcOffset.y * srcStep);
+          pDstT = (Ipp32f *)(pDst + dstOffset.y * dstStep);
 
           pOneBuf = pBuffer + i * bufSize1;
 
