1fab73e0f7fb12ac20ee3b14667db8541eba33d6
jvo203 <chris.zapart@nao.ac.jp>
2020-03-12 17:07:46 +0900

    modified:   src/fits.cpp

--- src/fits.cpp
+++ src/fits.cpp
@@ -3187,3 +3187,130 @@ IppStatus tileResize8u_C1R(Ipp8u *pSrc, IppiSize srcSize, Ipp32s srcStep,
 
   return status;
 }
+
+IppStatus tileResize8u_C1R_32f(Ipp32f *pSrc, IppiSize srcSize, Ipp32s srcStep,
+                               Ipp32f *pDst, IppiSize dstSize, Ipp32s dstStep) {
+
+  int MAX_NUM_THREADS = omp_get_max_threads();
+
+  IppiResizeSpec_32f *pSpec = 0;
+  int specSize = 0, initSize = 0, bufSize = 0;
+  Ipp8u *pBuffer = 0;
+  Ipp8u *pInitBuf = 0;
+  Ipp32u numChannels = 1;
+  IppiPoint dstOffset = {0, 0};
+  IppiPoint srcOffset = {0, 0};
+  IppStatus status = ippStsNoErr;
+  IppiBorderSize borderSize = {0, 0, 0, 0};
+  IppiBorderType border = ippBorderRepl;
+  int numThreads, slice, tail;
+  int bufSize1, bufSize2;
+  IppiSize dstTileSize, dstLastTileSize;
+  IppStatus pStatus[MAX_NUM_THREADS];
+
+  /* Spec and init buffer sizes */
+  status = ippiResizeGetSize_8u(srcSize, dstSize, ippLanczos, 0, &specSize,
+                                &initSize);
+
+  if (status != ippStsNoErr)
+    return status;
+
+  /* Memory allocation */
+  pInitBuf = ippsMalloc_8u(initSize);
+  pSpec = (IppiResizeSpec_32f *)ippsMalloc_8u(specSize);
+
+  if (pInitBuf == NULL || pSpec == NULL) {
+    ippsFree(pInitBuf);
+    ippsFree(pSpec);
+    return ippStsNoMemErr;
+  }
+
+  /* Filter initialization */
+  status = ippiResizeLanczosInit_8u(srcSize, dstSize, 3, pSpec, pInitBuf);
+  ippsFree(pInitBuf);
+
+  if (status != ippStsNoErr) {
+    ippsFree(pSpec);
+    return status;
+  }
+
+  status = ippiResizeGetBorderSize_8u(pSpec, &borderSize);
+  if (status != ippStsNoErr) {
+    ippsFree(pSpec);
+    return status;
+  }
+
+  /* General transform function */
+  /* Parallelized only by Y-direction here */
+#pragma omp parallel num_threads(MAX_NUM_THREADS)
+  {
+#pragma omp master
+    {
+      numThreads = omp_get_num_threads();
+      slice = dstSize.height / numThreads;
+      tail = dstSize.height % numThreads;
+
+      dstTileSize.width = dstLastTileSize.width = dstSize.width;
+      dstTileSize.height = slice;
+      dstLastTileSize.height = slice + tail;
+
+      ippiResizeGetBufferSize_8u(pSpec, dstTileSize, ippC1, &bufSize1);
+      ippiResizeGetBufferSize_8u(pSpec, dstLastTileSize, ippC1, &bufSize2);
+
+      pBuffer = ippsMalloc_8u(sizeof(Ipp32f) * bufSize1 * (numThreads - 1) +
+                              sizeof(Ipp32f) * bufSize2);
+    }
+
+#pragma omp barrier
+    {
+      if (pBuffer) {
+        Ipp32u i;
+        Ipp8u *pSrcT, *pDstT;
+        Ipp8u *pOneBuf;
+        IppiPoint srcOffset = {0, 0};
+        IppiPoint dstOffset = {0, 0};
+        IppiSize srcSizeT = srcSize;
+        IppiSize dstSizeT = dstTileSize;
+
+        i = omp_get_thread_num();
+        dstSizeT.height = slice;
+        dstOffset.y += i * slice;
+
+        if (i == numThreads - 1)
+          dstSizeT = dstLastTileSize;
+
+        pStatus[i] = ippiResizeGetSrcRoi_8u(pSpec, dstOffset, dstSizeT,
+                                            &srcOffset, &srcSizeT);
+
+        if (pStatus[i] == ippStsNoErr) {
+          pSrcT =
+              (Ipp8u *)((char *)pSrc + srcOffset.y * srcStep * sizeof(Ipp32f));
+          pDstT =
+              (Ipp8u *)((char *)pDst + dstOffset.y * dstStep * sizeof(Ipp32f));
+
+          pOneBuf = pBuffer + i * bufSize1 * sizeof(Ipp32f);
+
+          pStatus[i] = ippiResizeLanczos_32f_C1R(
+              (Ipp32f *)pSrcT, srcStep * sizeof(Ipp32f), (Ipp32f *)pDstT,
+              dstStep * sizeof(Ipp32f), dstOffset, dstSizeT, border, 0, pSpec,
+              pOneBuf);
+        }
+      }
+    }
+  }
+
+  ippsFree(pSpec);
+
+  if (pBuffer == NULL)
+    return ippStsNoMemErr;
+
+  ippsFree(pBuffer);
+
+  for (Ipp32u i = 0; i < numThreads; ++i) {
+    /* Return bad status */
+    if (pStatus[i] != ippStsNoErr)
+      return pStatus[i];
+  }
+
+  return status;
+}
