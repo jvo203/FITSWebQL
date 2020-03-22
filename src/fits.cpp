7c526370dc00cf6b03e34c0d4192f94ae844a219
jvo203 <chris.zapart@nao.ac.jp>
2020-03-22 12:21:12 +0900

    modified:   src/main.cpp

--- src/fits.cpp
+++ src/fits.cpp
@@ -2919,8 +2919,8 @@ IppStatus Resize_Invert_32f_C1R(Ipp32f *pSrc, IppiSize srcSize, Ipp32s srcStep,
       pOneBuf = pBuffer + i * bufSize1;
 
       pStatus[i] =
-          ippiResizeLanczos_32f_C1R(pSrcT, srcStep, pDstT,
-                                    dstStep , dstOffset,
+          ippiResizeLanczos_32f_C1R(pSrcT, srcStep * sizeof(Ipp32f), pDstT,
+                                    dstStep * sizeof(Ipp32f) , dstOffset,
                                     dstSizeT, border, pBorderValue, pSpec, pOneBuf);
     }
   }
