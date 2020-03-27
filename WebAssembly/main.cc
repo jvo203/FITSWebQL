e80e780f43c7a823dae0eaaf5a9b4fd287fcf877
jvo203 <chris.zapart@nao.ac.jp>
2020-03-27 14:34:23 +0900

    modified:   WebAssembly/main.cc

--- WebAssembly/main.cc
+++ WebAssembly/main.cc
@@ -5,10 +5,10 @@
 // colourmaps
 #include "colourmap.h"
 
-/*#include <OpenEXR/ImfInputFile.h>
 #include <OpenEXR/ImfChannelList.h>
 #include <OpenEXR/ImfHeader.h>
-#include <OpenEXR/ImfIO.h>*/
+#include <OpenEXR/ImfIO.h>
+#include <OpenEXR/ImfInputFile.h>
 
 #include <algorithm>
 #include <cstdint>
@@ -19,10 +19,132 @@
 
 using namespace emscripten;
 
-float lerp(float a, float b, float t) {
-    return (1 - t) * a + t * b;
+float lerp(float a, float b, float t) { return (1 - t) * a + t * b; }
+
+class BufferAdapter : public OPENEXR_IMF_NAMESPACE::IStream {
+public:
+  BufferAdapter(char *buffer, size_t size)
+      : IStream("BufferAdapter"), m_buffer(buffer), m_size(size), m_offset() {}
+
+  virtual bool isMemoryMapped() const override { return true; }
+
+  virtual bool read(char c[/*n*/], int n) override {
+    std::size_t remaining = m_size - m_offset;
+    if (std::size_t(n) > remaining)
+      throw std::runtime_error("Out of bounds read request");
+    auto current = readMemoryMapped(n);
+    std::copy(current, current + n, c);
+    return n == remaining;
+  }
+
+  virtual char *readMemoryMapped(int n) override {
+    std::size_t remaining = m_size - m_offset;
+    if (std::size_t(n) > remaining)
+      throw std::runtime_error("Out of bounds read request");
+    auto current = bufferCurrent();
+    m_offset += n;
+    return current;
+  }
+
+  virtual OPENEXR_IMF_NAMESPACE::Int64 tellg() override { return m_offset; }
+
+  virtual void seekg(OPENEXR_IMF_NAMESPACE::Int64 pos) override {
+    m_offset = pos;
+  }
+
+public:
+  char *bufferCurrent() const { return m_buffer + m_offset; }
+
+private:
+  char *m_buffer;
+  std::size_t m_size;
+  std::size_t m_offset;
+};
+
+typedef std::vector<char> Bytes;
+typedef std::vector<float> Pixel;
+typedef std::map<std::string, Pixel> Planes;
+
+struct EXRImage {
+  std::size_t width;
+  std::size_t height;
+  Planes planes;
+
+  emscripten::val plane(std::string const &name) const {
+    using namespace emscripten;
+    auto it = planes.find(name);
+    if (it == planes.end())
+      return val::undefined();
+    return val(typed_memory_view(it->second.size(), it->second.data()));
+  }
+
+  emscripten::val channels() const {
+    using namespace emscripten;
+    auto c = val::array();
+    std::size_t idx = 0;
+    for (auto const &plane : planes)
+      c.set(idx++, plane.first);
+    return val(c);
+  }
+};
+
+EXRImage loadEXRRaw(char const *buffer, std::size_t size) {
+  using namespace OPENEXR_IMF_NAMESPACE;
+  EXRImage image;
+  try {
+    BufferAdapter bytes(const_cast<char *>(buffer), size);
+    InputFile file(bytes);
+    auto window = file.header().dataWindow();
+    image.width = window.max.x - window.min.x + 1;
+    image.height = window.max.y - window.min.y + 1;
+    size_t strideX = sizeof(float);
+    size_t strideY = sizeof(float) * image.width;
+    FrameBuffer fb;
+    auto const &channels = file.header().channels();
+    auto it = channels.begin();
+    auto ed = channels.end();
+    for (; it != ed; ++it) // OpenEXR iterators are not real iterators so ranged
+                           // for loop doesn't work
+    {
+      auto const &name = it.name();
+      image.planes[name].resize(image.width * image.height);
+      // OpenEXR needs to be passed a pointer that, when the window is applied,
+      // leads to the start of the data buffer, even if that pointer is illegal
+      auto startOffset = window.min.x + window.min.y * image.width;
+      fb.insert(name, Slice(FLOAT,
+                            reinterpret_cast<char *>(image.planes[name].data() -
+                                                     startOffset),
+                            strideX, strideY));
+    }
+    file.setFrameBuffer(fb);
+    file.readPixels(window.min.y, window.max.y);
+  } catch (...) {
+  }
+  return image;
+}
+
+EXRImage loadEXRVec(std::vector<char> const &bytes) {
+  return loadEXRRaw(bytes.data(), bytes.size());
+}
+
+EXRImage loadEXRStr(std::string const &bytes) {
+
+  return loadEXRRaw(bytes.data(), bytes.size());
 }
 
 EMSCRIPTEN_BINDINGS(Wrapper) {
-    function("lerp", &lerp);
+  function("lerp", &lerp);
+
+  register_vector<float>("Pixel");
+  register_vector<char>("Bytes");
+  register_map<std::string, Pixel>("Planes");
+  class_<EXRImage>("EXRImage")
+      .property("width", &EXRImage::width)
+      .property("height", &EXRImage::height)
+      .property("planes", &EXRImage::planes)
+      .function("plane", &EXRImage::plane)
+      .function("channels", &EXRImage::channels);
+  function("loadEXRRaw", &loadEXRRaw, allow_raw_pointer<arg<0>>());
+  function("loadEXRVec", &loadEXRVec);
+  function("loadEXRStr", &loadEXRStr);
 }
