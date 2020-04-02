001d19fb5c4e5e393f181224fb06c17a65eaa9ff
jvo203 <chris.zapart@nao.ac.jp>
2020-04-02 16:09:07 +0900

    added gl.LINEAR with gl.FLOAT

--- htdocs2/fitswebql/fitswebql.js
+++ htdocs2/fitswebql/fitswebql.js
@@ -887,6 +887,9 @@ function webgl_renderer(index, gl, width, height) {
 	// load a texture
 	var tex = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, tex);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.LUMINANCE, image.img_width, image.img_height, 0, gl.LUMINANCE, gl.FLOAT, image.pixel);
 
 	//WebGL how to convert from clip space to pixels
