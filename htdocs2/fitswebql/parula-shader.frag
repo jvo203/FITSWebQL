36a1396ac3b077350b0031482fee131742896c0a
jvo203 <chris.zapart@nao.ac.jp>
2020-04-08 11:41:50 +0900

    added a parula colourmap shader

---
+++ htdocs2/fitswebql/parula-shader.frag
@@ -0,0 +1,3 @@
+    // red
+    gl_FragColor = colormap_parula(pixel, colour.a) ;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1473,6 +1473,10 @@ void http_fits_response(const response *res, std::vector<std::string> datasets,
   include_file(html, docs_root + "/fitswebql/rainbow-shader.frag");
   html.append("</script>\n");
 
+  html.append("<script id=\"parula-shader\" type=\"x-shader/x-vertex\">\n");
+  include_file(html, docs_root + "/fitswebql/parula-shader.frag");
+  html.append("</script>\n");
+
   // FITSWebQL main JavaScript + CSS
   html.append("<script src=\"fitswebql.js?" VERSION_STRING "\"></script>\n");
   html.append("<link rel=\"stylesheet\" href=\"fitswebql.css?" VERSION_STRING
