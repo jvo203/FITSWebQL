3b8812c059fa0c9e481d484b04d875e4b70e51aa
Christopher Zapart <chris.zapart@nao.ac.jp>
2020-08-11 16:43:37 +0900

    added a Julia program to make memory usage plots

--- Julia/memory_usage.jl
+++ Julia/memory_usage.jl
@@ -3,3 +3,9 @@ using Plots
 
 data = CSV.read("../memory_usage.csv")
 println(data[1:5,:])
+
+timestamp = data[:,1] ./ (1024 * 1024)
+allocated = data[:,2] ./ (1024 * 1024)
+
+plot(timestamp, allocated, label="jemalloc stats.allocated memory usage [MB]", xlabel="time [ms]", ylabel="memory [MB]")
+savefig("mem_allocated_v5.pdf")
