f61bdffbdea15fcac7a93e50eccc5c4e033228fe
Christopher Zapart <chris.zapart@nao.ac.jp>
2020-08-11 14:23:02 +0900

    modified:   src/main_uWS.cpp

--- src/main_uWS.cpp
+++ src/main_uWS.cpp
@@ -74,49 +74,8 @@ using namespace OPENEXR_IMF_NAMESPACE;
 #include <thread>
 #include <unordered_map>
 
+#ifdef DEBUG
 #include <jemalloc/jemalloc.h>
-#if !defined(__APPLE__) || !defined(__MACH__)
-//////////////////////////////////////////////////////////////////////////////
-//
-// process_mem_usage(double &, double &) - takes two doubles by reference,
-// attempts to read the system-dependent data for a process' virtual memory
-// size and resident set size, and return the results in KB.
-//
-// On failure, returns 0.0, 0.0
-
-void process_mem_usage(double &vm_usage, double &resident_set)
-{
-  using std::ifstream;
-  using std::ios_base;
-  using std::string;
-
-  vm_usage = 0.0;
-  resident_set = 0.0;
-
-  // 'file' stat seems to give the most reliable results
-  //
-  ifstream stat_stream("/proc/self/stat", ios_base::in);
-
-  // dummy vars for leading entries in stat that we don't care about
-  //
-  string pid, comm, state, ppid, pgrp, session, tty_nr;
-  string tpgid, flags, minflt, cminflt, majflt, cmajflt;
-  string utime, stime, cutime, cstime, priority, nice;
-  string O, itrealvalue, starttime;
-
-  // the two fields we want
-  //
-  unsigned long vsize;
-  long rss;
-
-  stat_stream >> pid >> comm >> state >> ppid >> pgrp >> session >> tty_nr >> tpgid >> flags >> minflt >> cminflt >> majflt >> cmajflt >> utime >> stime >> cutime >> cstime >> priority >> nice >> O >> itrealvalue >> starttime >> vsize >> rss; // don't care about the rest
-
-  stat_stream.close();
-
-  long page_size_kb = sysconf(_SC_PAGE_SIZE) / 1024; // in case x86-64 is configured to use 2MB pages
-  vm_usage = vsize / 1024.0;
-  resident_set = rss * page_size_kb;
-}
 #endif
 
 /** Thread safe cout class
@@ -149,7 +108,7 @@ std::mutex PrintThread::_mutexPrint{};
 
 std::atomic<bool> exiting(false);
 
-#if !defined(__APPLE__) || !defined(__MACH__)
+#ifdef DEBUG
 std::thread memory_thread;
 #endif
 
@@ -245,7 +204,7 @@ void signalHandler(int signum)
   }
 #endif
 
-#if !defined(__APPLE__) || !defined(__MACH__)
+#ifdef DEBUG
   memory_thread.join();
 #endif
 
@@ -1853,15 +1812,11 @@ void ipp_init()
 
 int main(int argc, char *argv[])
 {
-#if !defined(__APPLE__) || !defined(__MACH__)
+  #ifdef DEBUG
   // track/log memory usage
   memory_thread = std::thread([]() {
     while (!exiting)
     {
-      /*double vm, rss;
-      process_mem_usage(vm, rss);
-      std::cout << "VM: " << vm / (1024 * 1024) << " GB; RSS: " << rss / (1024 * 1024) << " GB" << std::endl;*/
-
       // memory statistics using jemalloc
       uint64_t epoch = 1;
       size_t sz = sizeof(epoch);
