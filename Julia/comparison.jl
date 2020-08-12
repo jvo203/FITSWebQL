bc1171072c31afcddc82537435585850bf6cd999
jvo203 <chris.zapart@nao.ac.jp>
2020-08-12 16:09:38 +0900

    a comparison in memory usage between v4 and v5

--- Julia/comparison.jl
+++ Julia/comparison.jl
@@ -5,12 +5,12 @@ data4 = CSV.read("../../fits_web_ql/memory_usage.csv")
 data5 = CSV.read("../memory_usage.csv")
 
 timestamp4 = data4[:,1] ./ 1000
-allocated4 = data4[:,2] ./ (1024 * 1024)
+allocated4 = data4[:,2] ./ (1024 * 1024 * 1024)
 
 timestamp5 = data5[:,1] ./ 1000
-allocated5 = data5[:,2] ./ (1024 * 1024)
+allocated5 = data5[:,2] ./ (1024 * 1024 * 1024)
 
 common = min(size(timestamp4)[1], size(timestamp5)[1])
 
-plot(timestamp5[1:common], [allocated4[1:common], allocated5[1:common]], label=["Rust fits_web_ql v4" "C/C++ FITSWebQL SE v5"], xlabel="elapsed time [s]", ylabel="jemalloc stats.allocated memory usage [MB]")
+plot(timestamp5[1:common], [allocated4[1:common], allocated5[1:common]], label=["Rust fits_web_ql v4" "C/C++ FITSWebQL v5"], xlabel="elapsed time [s]", ylabel="jemalloc stats.allocated memory usage [GB]")
 savefig("mem_two_way.pdf")
