da8e91e630ce2c964b96e2253fb73eb195033249
ザパート・クリストファー <chris.zapart@nao.ac.jp>
2020-08-13 10:06:08 +0900

    modified:   Julia/comparison.jl

--- Julia/comparison.jl
+++ Julia/comparison.jl
@@ -12,5 +12,5 @@ allocated5 = data5[:,2] ./ (1024 * 1024 * 1024)
 
 common = min(size(timestamp4)[1], size(timestamp5)[1])
 
-plot(timestamp5[1:common], [allocated4[1:common], allocated5[1:common]], label=["Rust fits_web_ql v4" "C/C++ FITSWebQL v5"], xlabel="elapsed time [s]", ylabel="jemalloc stats.allocated memory usage [GB]")
+plot(timestamp5[1:common], [allocated4[1:common], allocated5[1:common]], label=["Rust fits_web_ql v4" "C/C++ FITSWebQL v5"], xlabel="elapsed time [s]", ylabel="jemalloc stats.allocated memory [GB]")
 savefig("mem_two_way.pdf")
