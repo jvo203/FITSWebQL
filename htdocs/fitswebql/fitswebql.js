a0e2b30f477f37b644030319f61c78c6f2367107
jvo203 <chris.zapart@nao.ac.jp>
2020-08-17 17:32:32 +0900

    modified:   htdocs/fitswebql/fitswebql.js

--- htdocs/fitswebql/fitswebql.js
+++ htdocs/fitswebql/fitswebql.js
@@ -9318,7 +9318,7 @@ function setup_image_selection() {
 			init_webgl_zoom_buffers();
 
 			// send a "mouseenter" WebSocket message in order to reset the server-side Kalman Filter
-			var msg = 'mouseenter?seq_id=' + (++sent_seq_id);
+			var msg = 'kalman_reset?seq_id=' + (++sent_seq_id);
 			for (let index = 0; index < va_count; index++) {
 				if (wsConn[index].readyState == 1)
 					wsConn[index].send(msg);
@@ -9326,6 +9326,13 @@ function setup_image_selection() {
 		})
 		.on("mouseleave", function () {
 			clearTimeout(idleMouse);
+
+			// send a "mouseleave" WebSocket message in order to reset the server-side Kalman Filter
+			var msg = 'kalman_reset?seq_id=' + (++sent_seq_id);
+			for (let index = 0; index < va_count; index++) {
+				if (wsConn[index].readyState == 1)
+					wsConn[index].send(msg);
+			}
 
 			// clear the ViewportCanvas in WebGL
 			if (viewport != null) {
