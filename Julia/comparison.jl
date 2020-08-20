c5e30f145cc3efbe7ea76630a6ee39ab05c3be06
jvo203 <chris.zapart@nao.ac.jp>
2020-08-20 13:59:44 +0900

    modified:   Julia/comparison.jl

--- Julia/comparison.jl
+++ Julia/comparison.jl
@@ -5,10 +5,10 @@ data4 = CSV.read("../../fits_web_ql/memory_usage.csv")
 data5 = CSV.read("../memory_usage.csv")
 
 timestamp4 = data4[:,1] ./ 1000
-allocated4 = data4[:,2] ./ (1024 * 1024 * 1024)
+allocated4 = data4[:,2] ./ (1024^3)
 
 timestamp5 = data5[:,1] ./ 1000
-allocated5 = data5[:,2] ./ (1024 * 1024 * 1024)
+allocated5 = data5[:,2] ./ (1024^3)
 
 common = min(size(timestamp4)[1], size(timestamp5)[1])
 
