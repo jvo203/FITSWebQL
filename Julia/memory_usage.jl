1d4212ea787fe3aabb300a06eab3c1beeccf9d63
jvo203 <chris.zapart@nao.ac.jp>
2020-08-20 13:59:10 +0900

    modified:   Julia/memory_usage.jl

--- Julia/memory_usage.jl
+++ Julia/memory_usage.jl
@@ -5,7 +5,7 @@ data = CSV.read("../memory_usage.csv")
 println(data[1:5,:])
 
 timestamp = data[:,1] ./ 1000
-allocated = data[:,2] ./ (1024 * 1024)
+allocated = data[:,2] ./ (1024^3)
 
-plot(timestamp, allocated, label="jemalloc stats.allocated memory [MB]", xlabel="elapsed time [s]", ylabel="memory [MB]")
+plot(timestamp, allocated, label="jemalloc stats.allocated memory [GB]", xlabel="elapsed time [s]", ylabel="memory [MB]")
 savefig("mem_allocated_v5.pdf")
