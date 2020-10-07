68bd231c69a79cb9c86b62f281381c138e02f941
jvo203 <chris.zapart@nao.ac.jp>
2020-10-07 10:28:29 +0900

    modified:   src/fits.cpp

--- src/fits.cpp
+++ src/fits.cpp
@@ -4324,7 +4324,7 @@ void FITS::get_cube(int start, int end)
     shared(start_x, end_x, start_y, end_y)
   for (size_t i = (start - (start % 4)); i <= end; i++)
   {
-    // int tid = omp_get_thread_num();
+    int tid = omp_get_thread_num();
 
     // ZFP needs a chunk of 4 iterations per thread; <start> needs to be a
     // multiple of 4
@@ -4354,7 +4354,26 @@ void FITS::get_cube(int start, int end)
   jmp:
     if (!has_compressed_plane && fits_cube[i])
     {
-      // make_image_spectrumF32_ro
+      float fmin = FLT_MAX;
+      float fmax = -FLT_MAX;
+      float mean = 0.0f;
+      float integrated = 0.0f;
+
+      auto pixels_buf = fits_cube[i].get();
+
+      float _cdelt3 =
+        this->has_velocity
+        ? this->cdelt3 * this->frame_multiplier / 1000.0f
+        : 1.0f;
+
+      ispc::make_image_spectrumF32_ro(
+                (int32_t *)pixels_buf, bzero, bscale, ignrval,
+                datamin, datamax, _cdelt3, omp_pixels[tid], omp_mask[tid], fmin,
+                fmax, mean, integrated, plane_size);
+
+      
+      mean_spectrum[i - start] = mean;
+      integrated_spectrum[i - start] = integrated;
     }
   }
 
@@ -4394,6 +4413,12 @@ void FITS::get_cube(int start, int end)
     if (omp_mask[i] != NULL)
       ippsFree(omp_mask[i]);
   }
+
+// replace NaNs with 0.0
+#pragma omp parallel for simd
+    for (size_t i = 0; i < plane_size; i++)
+      if (_img_mask[i] == 0)
+        _img_pixels[i] = 0.0f;
 
   // return std::tuple with pixels, mask, mean_spectrum, integrated_spectrum
 }
