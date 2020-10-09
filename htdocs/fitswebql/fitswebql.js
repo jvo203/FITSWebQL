d094c5bfa09b21525e123c1c10c0a7565b4cc5c7
Christopher Zapart <chris.zapart@nao.ac.jp>
2020-10-09 14:59:49 +0900

    modified:   htdocs/fitswebql/fitswebql.js

--- htdocs/fitswebql/fitswebql.js
+++ htdocs/fitswebql/fitswebql.js
@@ -2695,10 +2695,13 @@ function open_websocket_connection(datasetId, index) {
 					//full spectrum refresh
 					if (type == 3) {
 						var length = dv.getUint32(12, endianness);
+						console.log("msg_type = 3: length = ", length);
 						var offset = 16;
 						var mean_spectrum = new Float32Array(received_msg, offset, length);
+						console.log(mean_spectrum);
 						offset += 4 * length + 4;
 						var integrated_spectrum = new Float32Array(received_msg, offset, length);
+						console.log(integrated_spectrum);
 
 						/*self.postMessage;console.log({type: 'refresh', latency: latency, recv_seq_id: recv_seq_id, length: length, mean_spectrum: mean_spectrum, integrated_spectrum: integrated_spectrum});*/
 
