f1ee988b8dcbe4a978f1e7b9151b790187b2c80f
jvo203 <chris.zapart@nao.ac.jp>
2020-10-21 11:41:39 +0900

    modified:   src/fits.cpp

--- src/fits.cpp
+++ src/fits.cpp
@@ -3766,6 +3766,22 @@ inline const char *FITS::check_null(const char *str)
     return "\"\"";
 };
 
+void FITS::update_histogram(Ipp32f *_pixels, Ipp8u *_mask, Ipp32f _min, Ipp32f _max)
+{
+  const size_t plane_size = width * height;
+
+  std::vector<Ipp32f> v(plane_size);
+
+  size_t len = 0;
+  for (size_t i = 0; i < plane_size; i++)
+    if (_mask[i] != 0)
+      v[len++] = _pixels[i];
+
+  v.resize(len);
+
+  std::lock_guard<std::mutex> guard(hist_mtx);
+}
+
 double FITS::make_median(Ipp32f *_pixels, Ipp8u *_mask)
 {
   const size_t plane_size = width * height;
