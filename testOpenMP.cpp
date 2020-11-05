2ea35db067a7f7be8e68e2565557afe4dc162ab3
ザパート・クリストファー <chris.zapart@nao.ac.jp>
2020-11-05 10:01:37 +0900

    new file:   testOpenMP.cpp

---
+++ testOpenMP.cpp
@@ -0,0 +1,34 @@
+#include <atomic>
+
+int main(int argc, char *argv[])
+{
+    int start_x = 0;
+    int start_y = 0;
+    int end_x = 5;
+    int end_y = 5;
+
+    std::atomic<bool> jmp = false;
+
+    #pragma omp parallel
+    {
+#pragma omp single
+      {
+        for (auto idy = start_y; idy <= end_y; idy++)
+        {
+          if (jmp)
+            break;
+
+          for (auto idx = start_x; idx <= end_x; idx++)
+          {
+            if (jmp)
+              break;
+
+#pragma omp task private(idx, idy)
+            {
+                printf("idx: %d, idy: %d\n", idx, idy);
+            }
+          }
+        }
+      }
+    }
+}
