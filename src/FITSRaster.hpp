d1e79a8476b45c2ae45ce70cbf8cad13adc8afce
jvo203 <chris.zapart@nao.ac.jp>
2020-11-09 15:27:10 +0900

    modified:   src/FITSRaster.hpp

--- src/FITSRaster.hpp
+++ src/FITSRaster.hpp
@@ -12,19 +12,41 @@ public:
         pixels = _pixels;
         width = _width;
         height = _height;
+        plane_size = width * height;
 
         if (pixels)
             data = pixels.get();
         else
-            data = NULL;            
+            data = NULL;
     }
+
     ~FITSRaster()
     {
         pixels.reset();
     };
 
+    // inherited virtual methods
+    virtual double value(double x, double y)
+    {
+        if (data == NULL)
+            return (0);
+
+        int idx = int(y) * width + int(x);
+
+        if (idx >= plane_size)
+            return (0);
+        else
+            return data[idx];
+    };
+
+    virtual SPoint upper_bound() { return (SPoint(width - 1, height - 1)); };
+    virtual SPoint lower_bound() { return (SPoint(0, 0)); };
+
+    // attributes
+private:
     std::shared_ptr<Ipp32f> pixels;
     Ipp32f *data;
     int width;
     int height;
+    int plane_size;
 };
