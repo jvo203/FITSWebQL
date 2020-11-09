71e20010c55270f30e350d3451f68de663cd627b
jvo203 <chris.zapart@nao.ac.jp>
2020-11-09 15:18:43 +0900

    modified:   src/FITSRaster.hpp

--- src/FITSRaster.hpp
+++ src/FITSRaster.hpp
@@ -2,6 +2,29 @@
 
 #include "contours.h"
 
+#include <ipp.h>
+
 class FITSRaster : CRaster
 {
+public:
+    FITSRaster(std::shared_ptr<Ipp32f> _pixels, int _width, int _height)
+    {
+        pixels = _pixels;
+        width = _width;
+        height = _height;
+
+        if (pixels)
+            data = pixels.get();
+        else
+            data = NULL;            
+    }
+    ~FITSRaster()
+    {
+        pixels.reset();
+    };
+
+    std::shared_ptr<Ipp32f> pixels;
+    Ipp32f *data;
+    int width;
+    int height;
 };
