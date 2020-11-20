aac5093c466896d1a1b46146130f298fd680650c
jvo203 <chris.zapart@nao.ac.jp>
2020-11-20 16:42:45 +0900

    modified:   WebAssembly/hevc_decoder.h

--- WebAssembly/hevc_decoder.h
+++ WebAssembly/hevc_decoder.h
@@ -1,4 +1,4 @@
-static void hevc_init(int va_count);
-static void hevc_destroy(int va_count);
-static double hevc_decode_nal_unit(int index, const unsigned char *data, size_t data_len, unsigned char *canvas, unsigned int _w, unsigned int _h, const unsigned char *alpha, unsigned char *bytes, const char *colourmap);
+void hevc_init(int va_count);
+void hevc_destroy(int va_count);
+double hevc_decode_nal_unit(int index, const unsigned char *data, size_t data_len, unsigned char *canvas, unsigned int _w, unsigned int _h, const unsigned char *alpha, unsigned char *bytes, const char *colourmap);
 
