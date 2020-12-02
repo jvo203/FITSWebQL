0380a048da4adef73e262ca5a5cb6cd8251788df
jvo203 <chris.zapart@nao.ac.jp>
2020-12-02 18:22:23 +0900

    modified:   src/fits.cpp

--- src/fits.cpp
+++ src/fits.cpp
@@ -4807,7 +4807,7 @@ FITS::get_video_frame(int frame, std::string flux)
             if (jmp)
               break;
 
-#pragma omp task private(idx, idy)
+#pragma omp private(idx, idy) task
             {
               // the on-demand decompression will be carried out in parallel
               std::shared_ptr<unsigned short> region =
