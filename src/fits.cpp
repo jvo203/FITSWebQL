dc20a08ee78cd517a9a49b904cba5a4997d531a9
jvo203 <chris.zapart@nao.ac.jp>
2020-12-02 10:30:34 +0900

    modified:   src/fits.cpp

--- src/fits.cpp
+++ src/fits.cpp
@@ -5247,7 +5247,8 @@ std::vector<float> FITS::get_spectrum(TWebSocket2 *ws, int start, int end, int x
   // std::lock_guard<std::mutex> guard(fits_mtx);
 
 #pragma omp parallel for schedule(dynamic, 4) shared(start_x, end_x, start_y, end_y)
-  for (size_t i = (start - (start % 4)); i <= end; i++)
+  //for (size_t i = (start - (start % 4)); i <= end; i++)
+  for (size_t i = start; i <= end; i++)
   {
     // int tid = omp_get_thread_num();
 
