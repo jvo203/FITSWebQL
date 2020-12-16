7a4e00bc6b793f0855da9fa17448844249d0441e
jvo203 <chris.zapart@nao.ac.jp>
2020-12-16 14:25:17 +0900

    modified:   src/global.h

--- src/global.h
+++ src/global.h
@@ -13,6 +13,7 @@
 
 using namespace std::chrono;
 
+#include <boost/lexical_cast.hpp>
 #include <boost/uuid/uuid.hpp>            // uuid class
 #include <boost/uuid/uuid_generators.hpp> // generators
 #include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
