da1372a67629f2d9915aed4f2b9aa03cd4c40147
jvo203 <chris.zapart@nao.ac.jp>
2020-12-24 14:08:50 +0900

    modified:   src/main_uWS.cpp

--- src/main_uWS.cpp
+++ src/main_uWS.cpp
@@ -984,7 +984,7 @@ void stream_partial_fits(uWS::HttpResponse<false> *res, std::shared_ptr<FITS> fi
     res->writeHeader("Content-Disposition", filename);
     res->writeHeader("Content-Transfer-Encoding", "binary");
     res->writeHeader("Accept-Ranges", "bytes");
-    res->writeHeader("Content-Range", "bytes 0-" + std::to_string(required_memory - 1) + "/" + std::to_string(required_memory));
+    //res->writeHeader("Content-Range", "bytes 0-" + std::to_string(required_memory - 1) + "/" + std::to_string(required_memory));
 
     char *header = (char *)malloc(fits->hdr_len);
 
@@ -1076,6 +1076,8 @@ void stream_partial_fits(uWS::HttpResponse<false> *res, std::shared_ptr<FITS> fi
 
       for (size_t frame = start; frame <= end; frame++)
       {
+        std::cout << "slicing through frame " << frame << std::endl;
+
         ssize_t bytes_read = 0;
 
         if (pixels_buf != nullptr)
@@ -1119,6 +1121,8 @@ void stream_partial_fits(uWS::HttpResponse<false> *res, std::shared_ptr<FITS> fi
 
       for (size_t frame = start; frame <= end; frame++)
       {
+        std::cout << "slicing through frame " << frame << std::endl;
+
         ssize_t bytes_read = 0;
 
         if (pixels_buf != NULL)
@@ -1148,6 +1152,8 @@ void stream_partial_fits(uWS::HttpResponse<false> *res, std::shared_ptr<FITS> fi
           break;
       }
     }
+
+    // send the 2880-bytes '\0' padding that might be needed
   }
 
 jmp:
