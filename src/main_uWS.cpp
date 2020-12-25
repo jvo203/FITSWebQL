3da336c22201da93d5771c7c1bbf9bbad5d9b24a
jvo203 <chris.zapart@nao.ac.jp>
2020-12-25 09:17:34 +0900

    modified:   src/main_uWS.cpp

--- src/main_uWS.cpp
+++ src/main_uWS.cpp
@@ -12,7 +12,7 @@
       VERSION_SUB)
 
 #define WASM_VERSION "20.11.27.2"
-#define VERSION_STRING "SV2020-12-24.0"
+#define VERSION_STRING "SV2020-12-25.0"
 
 // OpenEXR
 #include <OpenEXR/IlmThread.h>
